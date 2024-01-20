5516756569e1e8d72563155c6b8b220e42c63550
Philipe Rodrigues <dev.philipe@outlook.com>
2024-01-20 19:03:18 -0300

    test(municipe): Changes factory and creates RSpec feature tests

--- spec/factories/municipes.rb
+++ spec/factories/municipes.rb
@@ -7,9 +7,10 @@ FactoryBot.define do
     birth_date { FFaker::Date.birthday }
     phone_number { FFaker::PhoneNumberBR.international_mobile_phone_number }
 
-    random_image_file = Dir.glob(Rails.root.join('spec/fixtures/images/profiles/*.jpg').to_s).sample
-    after(:build) do |municipe|
+    before(:create) do |municipe|
+      random_image_file = Dir.glob(Rails.root.join('spec/fixtures/images/profiles/*.jpg').to_s).sample
       municipe.photo.attach(io: File.open(random_image_file), filename: 'profile.jpg')
     end
+    status { rand(0..1) }
   end
 end
