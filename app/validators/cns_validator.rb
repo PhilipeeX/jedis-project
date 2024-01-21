9ec6cf6e43cc377fe278b6d4b4fc91b12b7a105c
Philipe Rodrigues <dev.philipe@outlook.com>
2024-01-21 18:56:24 -0300

    refactor(cns_validator): Changes cns_valid? method

--- app/validators/cns_validator.rb
+++ app/validators/cns_validator.rb
@@ -8,39 +8,43 @@ class CnsValidator < ActiveModel::EachValidator
   private
 
   def cns_valid?(cns)
-    return false if cns.nil? || cns.strip.length != 15
+    return false unless cns && cns.strip.length == 15
 
-    sum = 0
     pis = cns[0, 11]
-    result = ''
+    sum = calculate_sum(pis)
 
+    dv = calculate_dv(sum)
+
+    result = if dv == 10
+               calculate_result(pis, 2)
+             else
+               "#{pis}000#{dv.to_i}"
+             end
+
+    cns == result
+  end
+
+  def calculate_sum(pis)
+    sum = 0
     pis.each_char.with_index do |digit, index|
       weight = 15 - index
       sum += digit.to_i * weight
     end
+    sum
+  end
 
+  def calculate_dv(sum)
     rest = sum % 11
     dv = 11 - rest
+    dv.zero? ? 0 : dv
+  end
 
-    dv = 0 if dv == 11
-
-    if dv == 10
-      sum = 0
-
-      pis.each_char.with_index do |digit, index|
-        weight = 15 - index
-        sum += digit.to_i * weight
-      end
-
-      sum += 2
-      rest = sum % 11
-      dv = 11 - rest
-      result = "#{pis}001#{dv.to_i}"
-    else
-      result = "#{pis}000#{dv.to_i}"
-    end
-
-    cns == result
+  def calculate_result(pis, extra_sum)
+    sum = calculate_sum(pis)
+    sum += extra_sum
+    rest = sum % 11
+    dv = 11 - rest
+    "#{pis}001#{dv.to_i}"
   end
 
   def prov_cns_valid?(cns)
