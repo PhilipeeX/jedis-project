b547a1b9d33be12c8e4e2f5618b15930471857df
Philipe Rodrigues <dev.philipe@outlook.com>
2024-01-22 11:30:37 -0300

    test(municipe): Creates RSpec feature tests for email error

--- spec/features/municipes_spec.rb
+++ spec/features/municipes_spec.rb
@@ -16,12 +16,14 @@ RSpec.feature 'Municipes', js: true, type: :feature do
   scenario 'create municipe' do
     visit new_municipe_path
     valid_cpf = '269.461.417-50'
+    valid_email = FFaker::Internet.free_email
 
     attach_file 'municipe_photo', 'spec/fixtures/images/profiles/perfil-masculino-2.jpg'
     fill_in('municipe[full_name]', with: 'Rick Spohr')
     fill_in('municipe[cpf]', with: valid_cpf)
     fill_in('municipe[cns]', with: '750620311660008')
-    fill_in('municipe_email', with: FFaker::Internet.free_email)
+    fill_in('municipe_email', with: valid_email)
+    fill_in('municipe_email_confirmation', with: valid_email)
     fill_in('municipe_birth_date', with: '1996-09-27')
     fill_in('municipe_phone_number', with: '5521966980366')
     select(I18n.t('activerecord.attributes.municipe.status.active'), from: 'municipe_status')
@@ -43,17 +45,35 @@ RSpec.feature 'Municipes', js: true, type: :feature do
 
   scenario 'update municipe' do
     valid_cpf = '822.420.720-05'
+    valid_email = FFaker::Internet.free_email
     visit municipes_path
     first('table tbody tr').click_link(I18n.t('municipes.municipe.edit'))
 
     fill_in 'municipe[full_name]', with: 'Lucas Borges'
     fill_in('municipe[cpf]', with: valid_cpf)
     fill_in('municipe[cns]', with: '838337659440018')
-    fill_in('municipe_email', with: FFaker::Internet.free_email)
+    fill_in('municipe_email', with: valid_email)
+    fill_in('municipe_email_confirmation', with: valid_email)
     click_button I18n.t('municipes.form.save')
 
     expect(page).to have_text('Lucas Borges')
     expect(page).to have_text('822.420.720-05')
     expect(page).to have_text('838337659440018')
+    expect(page).to have_text(valid_email)
+  end
+
+  scenario 'not update municipe with invalid email' do
+    valid_cpf = '822.420.720-05'
+    visit municipes_path
+    first('table tbody tr').click_link(I18n.t('municipes.municipe.edit'))
+
+    fill_in 'municipe[full_name]', with: 'Lucas Borges'
+    fill_in('municipe[cpf]', with: valid_cpf)
+    fill_in('municipe[cns]', with: '838337659440018')
+    fill_in('municipe_email', with: 'marcos_dev@gmail..com')
+    fill_in('municipe[email_confirmation]', with: 'marcos_dev@gmail..com')
+    click_button I18n.t('municipes.form.save')
+
+    expect(page).to have_text(I18n.t('errors.messages.invalid'))
   end
 end
