8ca4307763423173baa85b19e6f40216174262d4
Philipe Rodrigues <dev.philipe@outlook.com>
2024-01-24 08:19:34 -0300

    feat(address): Validates presence of mandatory fields

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :municipe
+  validates :cep, format: { with: /\A\d{5}-\d{3}\z/ }
+  validates :cep, :street, :neighborhood, :city, :state, presence: true
 end
